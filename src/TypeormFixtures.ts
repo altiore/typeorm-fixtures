6449d6bfc34e41784fca418bbf145579f4b4be6f
Razzwan <razvanlomov@gmail.com>
2018-09-20 12:11:23 +0300

    feat(chore): publish new version

--- src/TypeormFixtures.ts
+++ src/TypeormFixtures.ts
@@ -15,7 +15,11 @@ export class TypeormFixtures<AllFixtures = any[]> {
       const { findCondition, Entity } = this.findFixtures[findEntityName];
       currentRepo = this.connection.getRepository(Entity);
       this.entities[findEntityName] = await currentRepo.find(findCondition);
-      if (this.debug) console.log(`${findEntityName} fixture found`, this.entities[findEntityName]);
+      if (this.debug)
+        console.log(
+          `${TypeormFixtures.name}.loadFixtures: founded fixture ${findEntityName}`,
+          this.entities[findEntityName],
+        );
     }
     for (const entityName of Object.keys(this.fixtures)) {
       const { fixtures, Entity } = this.fixtures[entityName];
@@ -24,7 +28,8 @@ export class TypeormFixtures<AllFixtures = any[]> {
         return currentRepo.create(entity);
       });
       this.entities[entityName] = await currentRepo.save(preparedData);
-      if (this.debug) console.log(`${entityName} fixture added`, this.entities[entityName]);
+      if (this.debug)
+        console.log(`${TypeormFixtures.name}.loadFixtures: loaded fixture ${entityName}`, this.entities[entityName]);
     }
     return this.entities;
   };
@@ -35,9 +40,15 @@ export class TypeormFixtures<AllFixtures = any[]> {
       for (const entityName of Object.keys(this.fixtures)) {
         const { Entity } = this.fixtures[entityName];
         currentRepo = this.connection.getRepository(Entity);
-        await currentRepo.delete({ id: In(this.entities[entityName].map((el) => el.id)) });
-        if (this.debug) console.log(`${entityName} fixture dropped`);
-        delete this.entities[entityName];
+        if (this.entities[entityName]) {
+          await currentRepo.delete({ id: In(this.entities[entityName].map((el) => el.id)) });
+          delete this.entities[entityName];
+          if (this.debug) console.log(`${TypeormFixtures.name}.dropFixtures: ${entityName} dropped`);
+        } else {
+          const errorText = `${TypeormFixtures.name}.dropFixtures: Could not find entities by name ${entityName}`;
+          if (this.debug) console.log(errorText);
+          throw new Error(errorText);
+        }
       }
       await this.connection.close();
     } catch (e) {
@@ -54,11 +65,13 @@ export class TypeormFixtures<AllFixtures = any[]> {
     Entity: ObjectType<EntityType>;
   }) {
     this.fixtures[Entity.name] = { fixtures, Entity };
+    if (this.debug) console.log(`${Entity.name} -> ${TypeormFixtures.name}.addFixture`, { fixtures, Entity });
     return this;
   }
 
   public findEntities(findCondition, Entity) {
     this.findFixtures[Entity.name] = { findCondition, Entity };
+    if (this.debug) console.log(`${Entity.name} -> ${TypeormFixtures.name}.findEntities`, { findCondition, Entity });
     return this;
   }
 
@@ -72,5 +85,6 @@ export class TypeormFixtures<AllFixtures = any[]> {
     } else {
       this.connection = getConnection();
     }
+    if (this.debug) console.log(`${TypeormFixtures.name}.getConnection`);
   }
 }
--- src/getRelation.ts
+++ src/getRelation.ts
@@ -1,12 +1,8 @@
 import { isMatch, intersectionWith } from 'lodash';
+import { ObjectType } from 'typeorm';
 
-export function relation<EntityType>(
-  this: EntityType | EntityType[],
-  fixtureName: string,
-  entity,
-  relationName: string,
-): EntityType | EntityType[] {
-  return (this[fixtureName] && entity[relationName]
-    ? intersectionWith(this[fixtureName], entity[relationName], isMatch)
-    : this[fixtureName] || []) as EntityType[];
+export function relation<EntityType>(relationType: ObjectType<EntityType>, relation): EntityType[] {
+  return (this[relationType.name] && relation
+    ? intersectionWith(this[relationType.name], relation, isMatch)
+    : this[relationType.name] || []) as EntityType[];
 }
