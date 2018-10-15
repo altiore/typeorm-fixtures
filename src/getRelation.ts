6c6c11b6b9fdbea1798d10efef51c1c4ca494260
Razzwan <razvanlomov@gmail.com>
2018-10-15 11:25:44 +0300

    feat(getRelation): add one and many instead of relation

--- src/getRelation.ts
+++ src/getRelation.ts
@@ -1,6 +1,9 @@
 import { isMatch, intersectionWith } from 'lodash';
 import { ObjectType } from 'typeorm';
 
+/**
+ * @deprecated use many and one instead
+ */
 export function relation<EntityType>(
   allRelations: object,
   relationType: ObjectType<EntityType>,
@@ -9,4 +12,22 @@ export function relation<EntityType>(
   return (allRelations[relationType.name] && relation
     ? intersectionWith(allRelations[relationType.name], relation, isMatch)
     : allRelations[relationType.name] || []) as EntityType[];
+}
+
+export function many<EntityType>(allRelations: object, relationType: ObjectType<EntityType>, relation): EntityType[] {
+  return (allRelations[relationType.name] && relation
+    ? intersectionWith(allRelations[relationType.name], relation, isMatch)
+    : allRelations[relationType.name] || []) as EntityType[];
+}
+
+export function one<EntityType>(allRelations: object, relationType: ObjectType<EntityType>, relation): EntityType {
+  if (!allRelations[relationType.name]) {
+    throw new Error(
+      `Data for relation "${JSON.stringify(relation)}" is not set. Please load the fixture for it first!`,
+    );
+  }
+  if (!relation) {
+    throw new Error(`Relation data is required!`);
+  }
+  return allRelations[relationType.name].find((el: ObjectType<EntityType>) => isMatch(el, relation));
 }
